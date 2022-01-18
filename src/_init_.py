785c1d112e02cbc598cbb827364ad6c1203d8d32
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2022-01-18 11:50:57 -0600

    Interpolación de polinomios

--- src/_init_.py
+++ src/_init_.py
@@ -3,17 +3,54 @@ author = "Natalia Pérez Romero"
 version = "1.0"
 """
 import hashlib
+import secrets
+
+def horner_algorithm(x, n,coef):
+    result = 0
+    i=n-1
+    while(i >= 0):
+        result = (result * x) +coef[i]
+        i = i-1
+        
+    return result
+    
+def generate_coefficients(re,k):
+    coe = [h]
+    for i in range(1,re):
+        coe.append(secrets.randbelow(k)+1)
+    return coe
+
+def generate_evaluations(n,t,k,coef):
+    evaluations = []
+    for i in range(t):
+        x = secrets.randbelow(k)+1
+        tmp = horner_algorithm(x,n,coef)
+        evaluations.append(tmp)
+    return evaluations
+        
+    
+
+
 def cypher():
     #file = str(input())   # Archivo para guarda n evaluaciones del polinomio
-    #required_evaluations = int(input())  # num total de evaluaciones requeridas n>2
-    #min_point = int(input())   # num minimo de puntos necesarios para decifrar 1<t<n
+    #n = int(input())  # num total de evaluaciones requeridas n>2
+    #t = int(input())   # num minimo de puntos necesarios para decifrar 1<t<n
     #clean_file = str(input()) # nombre del archivo con el documento claro
+    print("Contraseña: ")
     password = str(input()).encode('utf-8')
-    print(password)
-    h = hashlib.sha256(password)
-    print(h.hexdigest())
+    h = hashlib.sha256(password).hexdigest()
+    k = int(h,16)
+    #coefficients = generate_coefficients(t+1,k) # Grado del polinomio
+    
+    k=20
+    prueba = [0,0,1]
+    ev = generate_evaluations(3,2,k,prueba)
+    
+    for i in ev:
+        print(i)
     
     
+
     
     
 def decipher():
@@ -22,10 +59,9 @@ def decipher():
     
     
     
-    
+print("c- cifrar \t d- descifrar  \t s-salir")
 option = input()
 while(option != "s"):
-    print("c- cifrar \t d- descifrar  \t s-salir")
     if(option == "c"):
         print("Cifrar")
         cypher()
@@ -34,7 +70,9 @@ while(option != "s"):
         decipher
     if(option == "s"):
         break
-    else: option = input()
+    else: 
+        print("c- cifrar \t d- descifrar  \t s-salir")
+        option = input()
     
     
 
